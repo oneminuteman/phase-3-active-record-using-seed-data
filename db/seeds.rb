3db3798b6c2b9b60fc44494ca42e4b64d543baea
Victor <victor.ireri@student.moringaschool.com>
2023-05-31 10:15:15 +0300

    labs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,18 @@
 # Add a console message so we can see output when the seed file runs
+require 'faker'
+
+# Add a console message so we can see output when the seed file runs
+puts "Seeding games..."
+
+# Run a loop to create 50 random games
+50.times do
+  Game.create(
+    title: Faker::Game.title,
+    genre: Faker::Game.genre,
+    platform: Faker::Game.platform,
+    price: rand(0..60)
+  )
+end
+
+puts "Done seeding!"
+
